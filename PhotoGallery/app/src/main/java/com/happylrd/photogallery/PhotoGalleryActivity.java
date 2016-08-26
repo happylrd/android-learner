293735f9acc1b8566ef785d63c6ced5d9ab61811
Diego Li <282108074@qq.com>
2016-08-26 16:38:56 +0800

    add background service
    use IntentService to practice Service.
    use AlarmManager to set a timer to delay running service and use
    PendingIntent to manager the timer.
    use Notifacation and NotificationManager to add notification info.

--- PhotoGallery/app/src/main/java/com/happylrd/photogallery/PhotoGalleryActivity.java
+++ PhotoGallery/app/src/main/java/com/happylrd/photogallery/PhotoGalleryActivity.java
@@ -1,9 +1,14 @@
 package com.happylrd.photogallery;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
-public class PhotoGalleryActivity extends SingleFragmentActivity  {
+public class PhotoGalleryActivity extends SingleFragmentActivity {
 
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
 
     @Override
     protected Fragment createFragment() {
